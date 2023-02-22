3c0543c45f2ead492fd3ea8d62ae16c0b5256bd1
Victor Ortega <victorortegalez@gmail.com>
2023-02-22 18:10:48 -0300

    Actualizar estilo de CartContainer

--- src/componentes/CartContainer/CartContainer.jsx
+++ src/componentes/CartContainer/CartContainer.jsx
@@ -20,76 +20,78 @@ const CartContainer = () => {
   console.log(total);
 
   return (
-    <div className="cartContainer-contenedor">
-      {cartList.map((producto) => (
-        <div className="itemCart" key={producto.id}>
-          <img src={producto.imagen} className="itemCart-img" />
+    <section className="cartContainer-section">
+      <div className="cartContainer-contenedor">
+        {cartList.map((producto) => (
+          <div className="itemCart" key={producto.id}>
+            <img src={producto.imagen} className="itemCart-img" />
 
-          <div className="itemCart-detalles">
-            {/* <h5>Detalles</h5> */}
-            <h4>{producto.nombre}</h4>
-            <p>{producto.detalle}.</p>
-          </div>
-
-          <div>
-            <h5>Precio</h5>
-            <h4>${producto.precio}</h4>
-          </div>
-
-          <div>
-            <h5>Sub-total</h5>
-            <h4>${producto.precio * producto.cantidad}</h4>
-          </div>
-
-          <div>
-            <h5>Cantidad</h5>
-            <div className="itemCart-cantidad">
-              <button
-                className="btn btn-outline-primary"
-                onClick={() => sumarCantidad(producto.id)}
-              >
-                +
-              </button>
-              <center>
-                <label>{producto.cantidad}</label>
-              </center>
-              <button
-                className="btn btn-outline-primary"
-                onClick={() => restarCantidad(producto.id)}
-              >
-                -
-              </button>
+            <div className="itemCart-detalles">
+              {/* <h5>Detalles</h5> */}
+              <h4>{producto.nombre}</h4>
+              <p>{producto.detalle}.</p>
             </div>
-          </div>
-          <img
-            src={trash}
-            style={{ width: "25px", height: "25px" }}
-            onClick={() => eliminarProducto(producto.id)}
-          ></img>
-          {/* <button
+
+            <div>
+              <h5>Precio</h5>
+              <h4>${producto.precio}</h4>
+            </div>
+
+            <div>
+              <h5>Sub-total</h5>
+              <h4>${producto.precio * producto.cantidad}</h4>
+            </div>
+
+            <div>
+              <h5>Cantidad</h5>
+              <div className="itemCart-cantidad">
+                <button
+                  className="btn btn-outline-primary"
+                  onClick={() => sumarCantidad(producto.id)}
+                >
+                  +
+                </button>
+                <center>
+                  <label>{producto.cantidad}</label>
+                </center>
+                <button
+                  className="btn btn-outline-primary"
+                  onClick={() => restarCantidad(producto.id)}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <img
+              src={trash}
+              style={{ width: "25px", height: "25px" }}
+              onClick={() => eliminarProducto(producto.id)}
+            ></img>
+            {/* <button
             className="btn btn-outline-danger"
             onClick={() => eliminarProducto(producto.id)}
           >
             Quitar producto
           </button> */}
-        </div>
-      ))}
-      <div className="itemCart-botones">
+          </div>
+        ))}
         <div>
           <button className="btn btn-outline-danger" onClick={vaciarCarrito}>
             Vaciar Carrito
           </button>
         </div>
-        <div className="itemCart-total">
-          <div>
-            <h4>Total: {total}</h4>
-          </div>
-          <div>
-            <button className="btn btn-outline-success">Comprar</button>
-          </div>
-        </div>
       </div>
-    </div>
+      <aside className="itemCart-total">
+        <div>
+          <h4>Sub-Total: ${total}</h4>
+          <h4>Total: ${total * 1.21}</h4>
+        </div>
+        <div>
+          <button className="btn btn-outline-success ">Comprar</button>
+          <button className="btn btn-outline-primary ">Seguir comprando</button>
+        </div>
+      </aside>
+    </section>
   );
 };
 
